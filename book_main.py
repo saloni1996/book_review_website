f088ac567d2c142d50c1759264aa6436deaf115d
saloni1996 <salonibhartia1996@gmail.com>
2017-03-07 11:05:47 +0530

    Added class along with defining its instances

---
+++ book_main.py
@@ -0,0 +1,7 @@
+class  Book():
+     def __init__(self,book_name,book_author,book_cover,author_interview,book_summary):
+         self.title =book_name
+         self.author =book_author
+         self.cover =book_cover
+         self.interview  =author_interview
+         self.summary = book_summary
---
+++ books_list.py
@@ -0,0 +1,12 @@
+import book_main as bm
+
+hp_1 = bm.Book('Harry Potter and Philospher Stone','J.K. Rowling',"https://goo.gl/images/vRlvZQ","https://www.youtube.com/watch?v=i_up1VTG7Fs","Book is about a young wizard Harry Potter and his adventures in wizarding world at the first year with his other wizard friends Hermoine and Ron ")
+
+hp_2 = bm.Book('Harry Potter and Chambers of Secret','J.K. Rowling',"https://goo.gl/images/qm5ZtW","https://www.youtube.com/watch?v=i_up1VTG7Fs","Book is about a young wizard Harry Potter and his adventures in wizarding world at his second year with his friends to unravel the mystery of Chambers of Secret")
+
+hp_3 = bm.Book('Harry Potter and Chambers of Secret','J.K. Rowling',"https://goo.gl/images/CRRJ32","https://www.youtube.com/watch?v=i_up1VTG7Fs","Book is about a young wizard Harry Potter and his adventures in wizarding world at his third year and is met with a new guest Sirius Black")
+
+
+
+
+print hp_3.summary
