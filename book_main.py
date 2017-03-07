76bfb18f6be134373615cfc5e52367901162f8b6
saloni1996 <salonibhartia1996@gmail.com>
2017-03-07 13:45:30 +0530

    Added new files with instances

--- book_main.py
+++ book_main.py
@@ -1,3 +1,5 @@
+import webbrowser
+
 class  Book():
      def __init__(self,book_name,book_author,book_cover,author_interview,book_summary):
          self.title =book_name
@@ -5,3 +7,5 @@ class  Book():
          self.cover =book_cover
          self.interview  =author_interview
          self.summary = book_summary
+     def show_interview(self):
+        webbrowser.open(self.interview)
--- books_list.py
+++ books_list.py
@@ -4,9 +4,18 @@ hp_1 = bm.Book('Harry Potter and Philospher Stone','J.K. Rowling',"https://goo.g
 
 hp_2 = bm.Book('Harry Potter and Chambers of Secret','J.K. Rowling',"https://goo.gl/images/qm5ZtW","https://www.youtube.com/watch?v=i_up1VTG7Fs","Book is about a young wizard Harry Potter and his adventures in wizarding world at his second year with his friends to unravel the mystery of Chambers of Secret")
 
-hp_3 = bm.Book('Harry Potter and Chambers of Secret','J.K. Rowling',"https://goo.gl/images/CRRJ32","https://www.youtube.com/watch?v=i_up1VTG7Fs","Book is about a young wizard Harry Potter and his adventures in wizarding world at his third year and is met with a new guest Sirius Black")
+hp_3 = bm.Book('Harry Potter and Prisoner of Azkaban','J.K. Rowling',"https://goo.gl/images/CRRJ32","https://www.youtube.com/watch?v=i_up1VTG7Fs","Book is about a young wizard Harry Potter and his adventures in wizarding world at his third year and is met with a new guest Sirius Black")
 
+hp_4 = bm.Book('Harry Potter and Goblet of Fire','J.K. Rowling',"https://goo.gl/images/rHUrfT","https://www.youtube.com/watch?v=i_up1VTG7Fs","Book is about a young wizard Harry Potter and his adventures in wizarding world at his fourth year and participates in Triwizard Cup")
 
+hp_5 = bm.Book('Harry Potter and Order of Phoenix','J.K. Rowling',"https://goo.gl/images/8wLdYt","https://www.youtube.com/watch?v=i_up1VTG7Fs","Book is about a young wizard Harry Potter and his adventures in wizarding world at his fifth year and fighting with Order of Phoenix")
 
+hp_6 = bm.Book('Harry Potter and Half Blood Prince','J.K. Rowling',"https://goo.gl/images/kscJa1","https://www.youtube.com/watch?v=i_up1VTG7Fs","Book is about a young wizard Harry Potter and his adventures in wizarding world at his sixth year and guided by Dumbledore")
 
-print hp_3.summary
+hp_7 = bm.Book('Harry Potter and Deathly Hallows','J.K. Rowling',"https://goo.gl/images/PBBVYi","https://www.youtube.com/watch?v=i_up1VTG7Fs","Harry Potter is all set to destroy Voldermort and his hocruxes")
+
+hp_8 = bm.Book('Harry Potter and Cursed Child','J.K. Rowling',"https://goo.gl/images/7RhZKF","https://www.youtube.com/watch?v=i_up1VTG7Fs","Epilogue of harry potter series with their children")
+
+hunger_games1 =bm.Book('Hunger Games','Sussane Collins',"https://goo.gl/images/WGG720","https://www.youtube.com/watch?v=tpCeDopdlwU","The story revolves around Panem, a depressed country, which ")
+
+hunger_games1.show_interview()
